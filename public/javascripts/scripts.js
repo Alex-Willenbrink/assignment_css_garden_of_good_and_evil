1d7dc334c08cd1c0804c91c2153f0dacbab69479
Greg Filipczak <gregfilipczak@gmail.com>
2017-07-24 21:18:08 +0000

    color change working

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,47 +1,48 @@
 console.log("Say this plz");
 
-
 let notParsed = document.cookie;
 let parsed = decodeURIComponent(document.cookie);
+console.log(parsed);
 
-if(parsed) {
-  
+if (parsed) {
 }
 
 parsed = JSON.parse(parsed.split("=")[1].substring(2));
+console.log(parsed);
 
-// { goodOrEvil, favFood, favColor } = parsed;
-// let favColor = "blue";
-//
-// // default background is white
-// let color = [255, 255, 255];
-//
-// $(document).ready(() => {
-//
-//   if(favColor) {
-//     switch (favColor){
-//       case blue:
-//         color[0] = 0;
-//         color[1] = 0;
-//         break;
-//       case green:
-//         color[0] = 0;
-//         color[2] = 0;
-//         break;
-//       case red:
-//         color[1] = 0;
-//         color[2] = 0;
-//         break;
-//     }
-//   }
-//
-//   if(goodOrEvil) {
-//     if (goodOrEvil === "evil") {
-//       color = color.map((ele) => {
-//         return ele / 3;
-//       });
-//     }
-//   }
-//   $("body").css("background", rgb(color));
-//
-// });
+let { goodOrEvil, favFood, favColor } = parsed;
+
+// default background is white
+let color = [255, 255, 255];
+
+$(document).ready(() => {
+  if (favColor) {
+    switch (favColor) {
+      case "blue":
+        color[0] = 0;
+        color[1] = 0;
+        break;
+      case "green":
+        color[0] = 0;
+        color[2] = 0;
+        break;
+      case "red":
+        color[1] = 0;
+        color[2] = 0;
+        break;
+    }
+  }
+
+  if (goodOrEvil) {
+    if (goodOrEvil === "evil") {
+      color = color.map(ele => {
+        return Math.floor(ele / 2);
+      });
+    }
+  }
+  $("body").css("background", `rgb(${color})`);
+
+  $(".dropdown-menu a").click(function() {
+    $("#selected").text($(this).text());
+  });
+});
