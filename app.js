e67152f19428ed24c848924257756a9aa96eab38
Alex-Willenbrink <awillenbrink@gmail.com>
2017-07-24 12:13:55 -0500

    cookie is working

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 const expressHandlebars = require("express-handlebars");
-
-const hbs = expressHandlebars.create({
-  defaultLayout: "main"
-  partialsDir: '/views'
-});
-
+const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.engine("handlebars", expressHandlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.use(express.static(`${__dirname}/public`));
 app.use(cookieParser());
 
-app.set("view engine", "handlebars");
-app.engine("handlebars", hbs.engine);
+app.get("/", (req, res) => {
+  res.end("Hello Sir");
+})
 
+app.get("/:id", (req, res) => {
+  const id = req.params.id;
+  const cookieArray = req.cookies.cookieArray || [];
 
+  cookieArray.push(id);
+
+  res.cookie("cookieArray", cookieArray, { maxAge: 90000, httpOnly: false });
+  console.log(req.cookies.cookieArray);
+  res.end();
+})
 
 
 app.listen(3000, "0.0.0.0", (req, res) => {
